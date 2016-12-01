5f6708018efb497b0f91f8c09496984221cdacff
NishinoNaru <zhuangyinwu@gmail.com>
2016-12-01 18:49:29 +0800

    1.0.0.programming.1.2016-1201-1847 // test jsoup

--- app/src/main/java/com/nishinonaru/devinfo/display/main/MainFragment.java
+++ app/src/main/java/com/nishinonaru/devinfo/display/main/MainFragment.java
@@ -14,9 +14,14 @@ import com.nishinonaru.devinfo.global.Const;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -54,17 +59,24 @@ public class MainFragment extends Fragment {
         return v;
     }
 
+    Document doc;
+
     private void testJsoup() {
         try {
-            Document doc = Jsoup.connect(Const.ADDRESS_ANDROID_CHINA).get();
-            Elements elements = doc.select("div.focus");
-            Log.d(TAG, "testJsoup: " + elements.select("a").attr("href"));
-
+            doc = Jsoup.connect(Const.ADDRESS_ANDROID_CHINA).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+        List<Map<String, String>> list = new ArrayList<>();
+        Elements elements = doc.getElementsByClass("focus");
+        for (Element e : elements) {
+            Map<String, String> map = new Hashtable<>();
+            map.put("title", e.getElementsByTag("a").select("img").attr("alt"));
+            list.add(map);
+        }
+        for (Map<String, String> map : list) {
+            Log.d(TAG, "testJsoup: " + map.get("title"));
+        }
     }
 
 
